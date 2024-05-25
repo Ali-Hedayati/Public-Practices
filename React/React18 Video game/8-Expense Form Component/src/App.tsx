369c5b1707f2a54f855a726e202d97dabf991631
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-05-25 13:43:57 +0330

    Form component add to "React 18 video game"

---
+++ React/React18 Video game/8-Expense Form Component/src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import ExpenseList from "./Components/ExpenseList";
+import { useState } from "react";
+import Filter from "./Components/Filter";
+import Form from "./Components/Form";
+export const categories = ["Cars", "Clothes", "Pens", "Phones", "Watches"];
+function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, name: "car", amount: 100, category: "Cars" },
+    { id: 2, name: "bag", amount: 98, category: "Clothes" },
+    { id: 3, name: "pen", amount: 250.69, category: "Pens" },
+    { id: 4, name: "ihpone", amount: 550, category: "Phones" },
+    { id: 5, name: "watch", amount: 1000, category: "Watches" },
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const rederList =
+    selectedCategory && selectedCategory !== "All"
+      ? expenses.filter((item) => item.category == selectedCategory)
+      : expenses;
+  return (
+    <>
+      <div className="mb-5">
+        <Form />
+      </div>
+      <div className="mb-3">
+        <Filter search={(category) => setSelectedCategory(category)} />
+      </div>
+      <ExpenseList
+        expenses={rederList}
+        Delete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ React/React18 Video game/8-Expense Form Component/src/Components/Form/index.tsx
@@ -0,0 +1,34 @@
+import { categories } from "../../App";
+
+const Form = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="string" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select className="form-select" id="category">
+          {categories.map((category) => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button className="btn btn-primary"> submit</button>
+    </form>
+  );
+};
+export default Form;
