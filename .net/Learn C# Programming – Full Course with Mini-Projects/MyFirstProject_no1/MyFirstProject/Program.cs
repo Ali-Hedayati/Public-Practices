844b1082f3451ea090c1263f8c68e080f79f6226
Ali Hedayati <a.hedayati.developer@gmail.com>
2026-07-07 14:14:18 +0330

    first commit for .net practice

---
+++ .net/Learn C# Programming – Full Course with Mini-Projects/MyFirstProject_no1/MyFirstProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            //if you delete using System; 
+            // you should use this instead
+            //System.Console.WriteLine("Hello world!");
+
+            Console.ReadLine(); //this will wait till user inputs somthing
+        }
+    }
+}
