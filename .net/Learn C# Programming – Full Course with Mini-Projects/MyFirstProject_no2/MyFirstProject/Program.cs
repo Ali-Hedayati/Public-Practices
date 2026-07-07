9228781a1051ce65f6c1f8f777b32b9f7986ed34
Ali Hedayati <a.hedayati.developer@gmail.com>
2026-07-07 17:39:19 +0330

    next lession

---
+++ .net/Learn C# Programming – Full Course with Mini-Projects/MyFirstProject_no2/MyFirstProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x=10 , y = 20 , z = 30 ; // another way of assign and create variables
+
+            int age = 23;
+            Console.WriteLine(age);
+
+            Console.WriteLine(int.MinValue); // it will show the min of an int
+            Console.WriteLine(int.MaxValue); // it will show the max of an int
+
+            long bigNumber = 90000L; // the "L" is for long
+            Console.WriteLine(bigNumber);
+
+            double negetive = 55.2D; // the "D" is for '.'
+            float precision = 5.0001F; // the "F" is for float
+
+            decimal money = 14.19M;//the "M" is for decimal
+
+            Console.ReadLine(); //this will wait till user inputs somthing
+        }
+    }
+}
